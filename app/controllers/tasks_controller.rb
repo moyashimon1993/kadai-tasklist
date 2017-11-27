ae241428aefd08861e22abf7a927277dfead382c
moyashi.tarou <moyashimon1993@gmail.com>
2017-11-27 13:14:59 +0000

    kadai user login

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,65 @@
+class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:new, :create]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    if logged_in?
+      @tasks = current_user.tasks.order(created_at: :desc)
+    end
+  end
+  
+  def create
+    @task = current_user.tasks.build(task_params)
+    
+    if @task.save
+      flash[:success]  = "タスクが投稿されました。"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクが投稿されませんでした"
+      render :new
+    end
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def update
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクは正常に更新されました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクは正常に更新されませんでした"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task.destroy
+    
+    flash[:success] = "タスクは正常に削除されました。"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def correct_user
+    redirect_to root_url if @task.user != current_user
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+end
